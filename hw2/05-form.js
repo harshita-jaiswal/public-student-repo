26ffb806627fcb667b795bf34b053926ff2d951a
hj7-HarshitaJaiswal <hj7@pdx.edu>
2023-10-14 21:53:06 -0700

    done exercise-05, form

--- hw2/05-form.js
+++ hw2/05-form.js
@@ -1,1 +1,25 @@
 // Add your code here
+let form = document.querySelector("form");
+
+form.onsubmit = (event) => {
+    event.preventDefault();
+    const selectedCheckboxes = document.querySelectorAll(
+        "input[type=checkbox]:checked"
+    );
+    if (form.elements.email.value && form.elements.fullName.value) {
+        console.group("========== Form Submission ========== ");
+        console.log("Full Name:", form.elements.fullName.value);
+        console.log("Email:", form.elements.email.value);
+        console.log("Registration Status:", form.elements.status.value);
+        console.log(
+            "Courses:",
+            Array.from(selectedCheckboxes)
+                .map((checkbox) => checkbox.value)
+                .join(", ")
+        );
+        console.log("Anything Else:", form.elements.moreInfo.value);
+        console.groupEnd();
+    } else {
+        console.warn("Please enter full name and email to submit the form");
+    }
+};
