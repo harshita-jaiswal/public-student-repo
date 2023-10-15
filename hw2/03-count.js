49e540bd80f67341cb30102bd550dcf4260a44f1
hj7-HarshitaJaiswal <hj7@pdx.edu>
2023-10-14 21:54:21 -0700

    completed count exercise

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,18 @@
 // Add your code here
+const elem = document.querySelector("input");
 
-input.addEventListener('keydown', handleKeyDown);
+const handleKeyDown = function handleInputKeyDown(event) {
+    const paragraphDiv = document.getElementById("paragraph");
+    const wordToHighlight = event.target.value;
+    const text = paragraphDiv.innerHTML;
+    const words = text.split(/\b/);
+    const highlightedWords = words.map((word) => {
+        if (word.toLowerCase() === wordToHighlight.toLowerCase()) {
+            return `<span class="bg-warning">${word}</span>`;
+        }
+        return word;
+    });
+
+    paragraphDiv.innerHTML = highlightedWords.join("");
+};
+elem.addEventListener("keydown", handleKeyDown);
