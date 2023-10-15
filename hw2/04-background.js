d40fe3b713429120e0db6be7e64317e08439c39e
hj7-HarshitaJaiswal <hj7@pdx.edu>
2023-10-14 21:52:16 -0700

    fix formatting

--- hw2/04-background.js
+++ hw2/04-background.js
@@ -2,25 +2,25 @@
 let isColorChanging = false;
 let intervalId = null;
 
-const startOrStopButton = document.getElementById('startOrStopBtn');
+const startOrStopButton = document.getElementById("startOrStopBtn");
 
 startOrStopButton.onclick = function onBtnClick() {
-    const intervalEle = document.querySelector('#interval');
+    const intervalEle = document.querySelector("#interval");
     const interval = intervalEle.value ? parseInt(intervalEle.value) : 3;
     if (isColorChanging) {
         clearInterval(intervalId);
         isColorChanging = false;
-        startOrStopButton.textContent = 'Start';
-        startOrStopButton.classList.remove('bg-danger');
-        intervalEle.removeAttribute('disabled');
+        startOrStopButton.textContent = "Start";
+        startOrStopButton.classList.remove("bg-danger");
+        intervalEle.removeAttribute("disabled");
     } else {
         intervalId = setInterval(changeBackgroundColor, interval * 1000);
         isColorChanging = true;
-        startOrStopButton.textContent = 'Stop';
-        startOrStopButton.classList.add('bg-danger');
-        intervalEle.setAttribute('disabled', true);
+        startOrStopButton.textContent = "Stop";
+        startOrStopButton.classList.add("bg-danger");
+        intervalEle.setAttribute("disabled", true);
     }
-}
+};
 
 function changeBackgroundColor() {
     const r = Math.floor(Math.random() * 256);
